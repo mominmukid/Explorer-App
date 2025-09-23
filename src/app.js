fac404e98ae410dd82a9970eb0dc6e565cdfd528
mominmukid <mominmukid@gmail.com>
2025-09-23 20:55:59 +0530

    fix: Correct start script path in package.json and add root route in app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookiesparcer());
-
+ app.get("/",(req,res)=>{
+  res.send("Hello this is the Backend of Explorer APP")
+ })
 //routes  import
 import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
