0e6c02cc7031aea8c74a3dc27c9057708806f795
mominmukid <mominmukid@gmail.com>
2025-09-23 23:29:26 +0530

    refactor: Restructure application entry points by moving server setup to server.js and updating script paths in package.json

--- src/app.js
+++ src/app.js
@@ -1,37 +1,16 @@
-import express from "express";
-import cors from "cors";
-import cookiesparcer from "cookie-parser";
-const app = express();
-// this is the standard middleware for CORS
-app.use(cors({
-  origin: "http://localhost:5173", // ✅ your frontend URL
-  credentials: true,               // ✅ allow cookies
-}));
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./server.js";
 
-// this is the standard middleware for parsing JSON requests
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookiesparcer());
- app.get("/",(req,res)=>{
-  res.send("Hello this is the Backend of Explorer APP")
- })
-//routes  import
-import userRouter from "./routes/user.routes.js";
-import tweetRouter from "./routes/tweet.routes.js";
-import videoRouter from "./routes/video.routes.js";
-import playListRouter from "./routes/playlist.routes.js";
-import likeRouter from "./routes/like.routes.js";
-import commentRouter from "./routes/comment.routes.js";
-import subscribRouter from "./routes/subcription.routes.js";
-
-//routes decleration
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/tweet", tweetRouter);
-app.use("/api/v1/videos", videoRouter);
-app.use("/api/v1/playlist", playListRouter);
-app.use("/api/v1/like", likeRouter);
-app.use("/api/v1/comment", commentRouter);
-app.use("/api/v1/subscribe", subscribRouter);
-
-export default app;
+// this is the standard middleware for handling errors
+dotenv.config();
+ 
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server start on port ${process.env.PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log("MONGODB connection error !!!!");
+  });
