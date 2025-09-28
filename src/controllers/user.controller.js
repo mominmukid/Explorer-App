4a758880233e6bb256ab80ef91cbfd0571075256
mominmukid <mominmukid@gmail.com>
2025-09-28 17:22:12 +0530

    Add user registration and login enhancements
    - Introduced separate checks for existing email and username during user registration.
    - Updated error messages for clarity: "Email already existed" and "Username already existed".
    - Enhanced cookie options for access and refresh tokens to improve security.
    - Modified login error messages for better user feedback: "User Email not found" and "Invalid Password".
    - Added new image files to the public/temp directory.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -45,11 +45,17 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // check if user already exists
-  const existedUser = await User.findOne({
-    $or: [{ email }, { password }],
+  const existedUseremail = await User.findOne({
+    $or: [{ email }],
   });
-  if (existedUser) {
-    throw new ApiError(409, "User already existed");
+  if (existedUseremail) {
+    throw new ApiError(409, "Email already existed");
+  }
+  const existedUserName = await User.findOne({
+    $or: [{ username }],
+  });
+  if (existedUserName) {
+    throw new ApiError(409, "Username already existed");
   }
 
   // get avatar and cover image paths from request using multer
@@ -99,6 +105,12 @@ const registerUser = asyncHandler(async (req, res) => {
   const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
     user._id
   );
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // only true in prod
+    sameSite: "none", // for cross-site (frontend & backend different domains)
+    path: "/",
+  };
 
   return res
     .status(201)
@@ -119,12 +131,12 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
   if (!user) {
-    throw new ApiError(404, "User not found");
+    throw new ApiError(404, "User Email not found");
   }
 
   const isPasswordValid = await user.isPasswordCorrect(password);
   if (!isPasswordValid) {
-    throw new ApiError(401, "Invalid credentials");
+    throw new ApiError(401, "Invalid Password");
   }
 
   const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
