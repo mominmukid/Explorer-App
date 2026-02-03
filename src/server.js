110a3d0c20625bb378e8a7315034efdc934459a1
Mukid Momin <mominmukid@gmail.com>
2026-02-03 13:51:16 +0530

    refactor: Update server connection logic to use async MongoDB connection handling

--- src/server.js
+++ src/server.js
@@ -6,13 +6,30 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
+let isconected = false;
+ async function connectMongoDB() {
+  try {
+    await connectDB();
+    isconected = true;
+    console.log("✅ MONGODB connected successfully");
+  } catch (error) {
     console.error("❌ MONGODB connection error:", error.message);
-    process.exit(1); // crash the process in production if DB fails
-  });
+    isconected = false;
+  }
+ }
+app.use(async (req, res, next) => {
+  if (!isconected) {
+    await connectMongoDB();
+    next();
+  }});
+  module.exports = app;
+// connectDB()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`🚀 Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("❌ MONGODB connection error:", error.message);
+//     process.exit(1); // crash the process in production if DB fails
+//   });
